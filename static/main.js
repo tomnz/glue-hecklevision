787a0b654393a73dc5c424a0e3b5a46790dccaf8
Tom Mitchell <tmitchell@hioscar.com>
2020-03-19 00:56:55 -0700

    Rework to show multiple messages

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-const MESSAGE_POLL_MS = 2000;
+const MESSAGE_POLL_MS = 1000;
 const MESSAGE_SHOW_MS = 20000;
 
 let lastTimestamp = new Date().getTime() / 1000;
@@ -7,72 +7,36 @@ const fetchMessages = async after => {
   return fetch(`/get?after=${after}`);
 };
 
-const messages = [];
-let showing = false;
-
-const triggerMessage = () => {
-  // Future calls to showMessage function are automated with a timeout, so
-  // ignore if it's already active
-  if (showing || messages.length === 0) {
-    return;
-  }
-  showMessage();
-};
-
-const showMessage = () => {
-  // Manual DOM manipulation because YOLO
-  const containerEl = document.getElementById('container');
-  const messageEl = document.getElementById('message');
-  const messagesRemainingEl = document.getElementById('messagesRemaining');
-
-  // No messages left? Clean up
-  if (messages.length === 0) {
-    messageEl.innerHTML = '';
-    containerEl.classList.add('empty');
-    showing = false;
-    return;
-  }
-
-  // We have some messages! Get it going
-  containerEl.classList.remove('empty');
-  showing = true;
-
-  const message = messages.shift();
-
-  const authorEl = document.createElement('span');
-  authorEl.innerText = message.author;
-  authorEl.classList.add('messageAuthor');
-
-  const textEl = document.createElement('span');
-  textEl.innerText = message.text;
-  textEl.classList.add('messageText');
-
-  // Show the message, overwriting any existing
-  messageEl.innerHTML = '';
-  messageEl.appendChild(authorEl);
-  messageEl.appendChild(document.createTextNode(': '));
-  messageEl.appendChild(textEl);
-
-  // Update the remaining count
-  if (messages.length > 0) {
-    messagesRemainingEl.innerText = `${messages.length} queued`
-  } else {
-    messagesRemainingEl.innerHTML = '';
-  }
-
-  // Schedule the next message
-  setTimeout(showMessage, MESSAGE_SHOW_MS);
-};
-
 const updateMessages = () => {
+  const messagesEl = document.getElementById('messages');
+
   fetchMessages(lastTimestamp).then((resp) => resp.json()).then((data) => {
     data.forEach((message) => {
       if (message.timestamp > lastTimestamp) {
         lastTimestamp = message.timestamp;
       }
-      messages.push(message);
+
+      const authorEl = document.createElement('span');
+      authorEl.innerText = message.author;
+      authorEl.classList.add('messageAuthor');
+
+      const textEl = document.createElement('span');
+      textEl.innerText = message.text;
+      textEl.classList.add('messageText');
+
+      const messageEl = document.createElement('div');
+      messageEl.classList.add('message');
+
+      messageEl.appendChild(authorEl);
+      messageEl.appendChild(document.createTextNode(': '));
+      messageEl.appendChild(textEl);
+
+      messagesEl.insertBefore(messageEl, messagesEl.children[0]);
+
+      setTimeout(() => {
+        messagesEl.removeChild(messageEl);
+      }, MESSAGE_SHOW_MS)
     });
-    triggerMessage();
   });
 
   // Schedule the next update
