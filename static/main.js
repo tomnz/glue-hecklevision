a1cbdb13783d56841cd499d93485df90e82034c8
Tom Mitchell <tmitchell@hioscar.com>
2020-03-19 01:35:08 -0700

    Generate color based on username

--- static/main.js
+++ static/main.js
@@ -3,6 +3,15 @@ const MESSAGE_SHOW_MS = 20000;
 
 let lastTimestamp = new Date().getTime() / 1000;
 
+const stringToColor = (str) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const hue = (hash >> 7) % 360;
+  return `hsl(${hue}, 80%, 70%)`;
+};
+
 const fetchMessages = async after => {
   return fetch(`/get?after=${after}`);
 };
@@ -23,6 +32,8 @@ const updateMessages = () => {
       const textEl = document.createElement('span');
       textEl.innerText = message.text;
       textEl.classList.add('messageText');
+      // Generate a random color based on username, because why not
+      textEl.style.color = stringToColor(message.author);
 
       const messageEl = document.createElement('div');
       messageEl.classList.add('message');
